e36e3d2ec2ae2f7f87567a7b5d373e785e83cdfd
Boo-coder <Bouna112@yahoo.com>
2025-09-01 22:34:07 -0400

    extends

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Your code here
+class Polygon{
+    constructor(array){
+        this.array = array;
+    }
+
+    get countSides(){
+        return this.array.length;
+    }
+
+    get perimeter(){
+        return this.array.reduce((acc, currentValue) => acc + currentValue, 0);
+    }
+}
+
+
+class Triangle extends Polygon {
+  get isValid() {
+    if (this.countSides !== 3) return false;
+    const [a, b, c] = this.array;
+    return a + b > c && a + c > b && b + c > a;
+  }
+}
+
+class Square extends Polygon {
+  get isValid() {
+    if (this.countSides !== 4) return false;
+
+    const [a, b, c, d] = this.array;
+    return a === b && b === c && c === d;
+  }
+
+  get area() {
+    if (this.isValid) {
+      return this.array[0] ** 2;
+    }
+    return null;
+  }
+}
